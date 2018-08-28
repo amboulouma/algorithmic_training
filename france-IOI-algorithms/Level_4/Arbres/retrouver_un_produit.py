f6cc7cda87e4bd2bd35e83295381bcc45cb364f6
Amine M. Boulouma <amine+socialmedias@boulouma.com>
2018-08-28 23:24:09 +0200

    Update retrouver_un_produit.py

--- france-IOI-algorithms/Level_4/Arbres/retrouver_un_produit.py
+++ france-IOI-algorithms/Level_4/Arbres/retrouver_un_produit.py
@@ -2,12 +2,9 @@ n = int(input())
 code_pos = list(map(int, input().split()))
 r = int(input())
 codes = list(map(int, input().split()))
-
 containers = []
-
 for i in range(n):
     if i + 1 not in code_pos:
         containers.add(code_pos[i])
-
 for i in range(r):
     print()
