db6d692da538bc20d73692c3197d6ad0cbe85883
Amine M. Boulouma <amine@boulouma.com>
2018-10-11 12:07:02 +0200

    added leetcode challenge roman

---
+++ leetcode/unluck_13.py
@@ -0,0 +1,10 @@
+numbers = list(map(int, input().split()))
+times_unlucky = len([i for i in numbers if i == 13])
+print(times_unlucky)
+for i in range(times_unlucky):
+    index_unlucky = numbers.index(13)
+    if index_unlucky != len(numbers)-1:
+        del numbers[index_unlucky+1]
+    del numbers[index_unlucky]
+print(numbers)
+print(sum(numbers))
