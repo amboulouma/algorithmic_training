760113e39e27de109014d7d5e5824243fcb50ecd
Amine M. Boulouma <amine@boulouma.com>
2018-10-11 12:17:18 +0200

    added pythoning challenge unlucky 13

--- pythoning/unluck_13.py
+++ pythoning/unluck_13.py
@@ -1,6 +1,5 @@
 numbers = list(map(int, input().split()))
 times_unlucky = len([i for i in numbers if i == 13])
-print(times_unlucky)
 for i in range(times_unlucky):
     index_unlucky = numbers.index(13)
     if index_unlucky != len(numbers)-1:
